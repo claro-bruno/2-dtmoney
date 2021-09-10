74f06804a48f46fc7ea0a1ffc83933bcee10d4bd
Bruno Augusto Claro <brunaugusto@gmail.com>
2021-09-10 03:25:51 -0300

    Dashboard Summary Components

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,35 @@
+import IncomeImg from '../../assets/income.svg';
+import OutcomeImg from '../../assets/outcome.svg';
+import TotalImg from '../../assets/total.svg';
+
+import { Container } from "./styles";
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={ IncomeImg } alt="Entradas" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={ OutcomeImg } alt="Saidas" />
+        </header>
+        <strong>- R$500,00</strong>
+      </div>
+
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={ TotalImg } alt="Total" />
+        </header>
+        <strong>R$500,00</strong>
+      </div>
+    </Container>
+  );
+}
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,6 +7,7 @@ export const GlobalStyle = createGlobalStyle`
   --red: #e52e4d;
   --blue: #5429cc;
   --blue-light: #6933ff;
+  --green: #33cc95;
 
   --text-title: #363f5f;
   --text-body: #969cb3;
