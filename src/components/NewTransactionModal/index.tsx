735f64f0326fa8d199962f5fa344143c56cde1df
Bruno Augusto Claro <brunaugusto@gmail.com>
2021-09-11 01:42:12 -0300

    Created Typee Buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -22,6 +24,21 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
           <h2>Cadastrar Transação</h2>
             <input placeholder="Título" />
             <input placeholder="Valor" type="number" />
+            <TransactionTypeContainer>
+              <button
+                type="button"
+              >
+                <img src={ incomeImg } alt="Entrada" />
+                <span>Entrada</span>
+              </button>
+
+              <button
+                type="button"
+              >
+                <img src={ outcomeImg } alt="Saída" />
+                <span>Saída</span>
+              </button>
+            </TransactionTypeContainer>
             <input placeholder="Categoria" />
             <button type="submit">
               Cadastrar 
